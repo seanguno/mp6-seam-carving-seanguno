35ce4212ac488091d5f458a73423f2571785fe92
Sean Guno <91289730+seanguno@users.noreply.github.com>
2023-09-10 13:01:37 -0500

    Update image_ppm.cc

--- src/image_ppm.cc
+++ src/image_ppm.cc
@@ -86,3 +86,72 @@ std::istream& operator>>(std::istream& is, ImagePPM& image) {
   }
   return is;
 }
+
+// returns the Pixel at row col. You may assume that row and col
+  // will always be within the bounds of image_
+  Pixel ImagePPM::GetPixel(int row, int col) const {
+    return pixels_[row][col];
+  }
+
+  // returns the max color value of the image
+  int ImagePPM::GetMaxColorValue() const {
+    return max_color_value_;
+  }
+
+  // outputs the image in plain PPM format to os
+  std::ostream& operator<<(std::ostream& os, const ImagePPM& image) {
+    os << "P3" << "\n";
+    os << std::to_string(image.GetWidth()) + " " + std::to_string(image.GetHeight()) << "\n";
+    os << std::to_string(image.GetMaxColorValue()) + "\n";
+    for (int row = 0; row < image.GetHeight(); row++) {
+      for (int col = 0; col < image.GetWidth(); col++) {
+        os << std::to_string(image.GetPixel(row, col).GetRed()) + "\n" + std::to_string(image.GetPixel(row, col).GetGreen()) + "\n" + std::to_string(image.GetPixel(row, col).GetBlue()) + "\n";      }
+    }
+    return os;
+  }
+
+  void ImagePPM::RemoveVert(const int* vert_seam) {
+    auto** to_replace = new Pixel*[height_];
+    for (int row = 0; row < height_; ++row) {
+      to_replace[row] = new Pixel[width_];
+    }
+    int new_image = 0;
+    for (int row = 0; row < height_; row++) {
+      for (int col = 0; col < width_; col++) {
+        if (col != vert_seam[row]) {
+          to_replace[row][new_image] = GetPixel(row, col);
+          new_image++;
+        }
+      }
+      new_image = 0;
+    }
+    for (int i = 0; i < height_; i++) {
+      delete[] pixels_[i];
+    }
+    delete[] pixels_;
+    pixels_ = to_replace;
+    width_--;
+  }
+
+  void ImagePPM::RemoveHoriz(const int* horiz_seam) {
+    auto** to_replace = new Pixel*[height_ - 1];
+    for (int row = 0; row < height_ - 1; ++row) {
+      to_replace[row] = new Pixel[width_];
+    }
+    int new_image = 0;
+    for (int col = 0; col < width_; col++) {
+      for (int row = 0; row < height_; row++) {
+        if (row != horiz_seam[col]) {
+          to_replace[new_image][col] = GetPixel(row,col);
+          new_image++;
+        }
+      }
+      new_image = 0;
+    }
+    for (int i = 0; i < height_; i++) {
+      delete[] pixels_[i];
+    }
+    delete[] pixels_;
+    pixels_ = to_replace;
+    height_--;
+  }
